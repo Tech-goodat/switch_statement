cd9e2c7c69381f83430230c32a62e019d02d2e35
Tech-goodat <felixkiprotich2000@gmail.com>
2023-02-01 11:48:48 +0300

    added a switch statement to scan for a date

---
+++ app.js
@@ -0,0 +1,25 @@
+//switch statement to get the day of the week
+switch (new Date().getDay()){
+    case 0:
+        day="sunday";
+        break;
+        case 1:
+        day="monday";
+        break;
+        case 2:
+        day="tuesday";
+        break;
+        case 3:
+        day="wednesday";
+        break;
+        case 4:
+        day="thursday";
+        break;
+        case 5:
+        day="friday";
+        break;
+        case 6:
+        day="saturday";
+        break;
+        
+}
